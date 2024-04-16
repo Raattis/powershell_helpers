be38196c37d94c779d3f382aef1799e8e9fd2351
Riku Rajaniemi <riku.rajaniemi@ubisoft.com>
2024-04-16 14:50:48 +0300

    Use 'contains' filtering by default. Exclude __pycache__ by default.

--- f.ps1
+++ f.ps1
@@ -1,22 +1,28 @@
 param (
-    [Parameter(Position = 0, Mandatory = $false)]
-    [string]$filter = "*",
+	[Parameter(Position = 0, Mandatory = $false)]
+	[string]$filter = "*",
 
-    [Parameter(Position = 1, Mandatory = $false)]
-    [string]$pattern,
-	
-    [Parameter(Mandatory = $false)]
-    [string[]]$exclude = @("*\.git\*", "*\build\*", "*\.vs\*"),
+	[Parameter(Position = 1, Mandatory = $false)]
+	[string]$pattern,
 
-    [Parameter(Mandatory = $false)]
-    [string]$path = "."
+	[Parameter(Mandatory = $false)]
+	[string[]]$exclude = @("*\.git\*", "*\build\*", "*\.vs\*", "*\__pycache__\*"),
+
+	[Parameter(Mandatory = $false)]
+	[string]$path = "."
 )
+
+if (($filter -like "*!") -or ($filter -like ("*" + [wildcardpattern]::Escape("*") + "*"))) {
+	$filter = $filter -replace "!"
+} elseif (-not $exact) {
+	$filter = "*" + $filter + "*"
+}
 
 $exclude_root = foreach($ex in $exclude) { $ex -replace "\*\\" -replace "\\\*" }
 $paths = ls $path -force -exclude $exclude_root
 $items = foreach ($p in $paths) {
 	if (Test-Path $p -PathType Leaf) {
-		if ([string]::IsNullOrWhiteSpace($filter) -or $p -like $filter) {
+		if ([string]::IsNullOrWhiteSpace($filter) -or ((split-path -leaf $p) -like $filter)) {
 			$p
 		}
 	} else {
